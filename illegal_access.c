a37cc98a7119b3cfe93ee3d39108633bcab673d1
Michiel Staessen <staessenmichiel@gmail.com>
2013-01-27 22:09:37 +0100

    4. Functions and Contracts

--- illegal_access.c
+++ illegal_access.c
@@ -4,16 +4,39 @@ struct account {
     int balance;
 };
 
-int main()
-//@ requires true;
-//@ ensures true;
+struct account *account_create()
+    //@ requires true;
+    //@ ensures malloc_block_account(result) &*& result->balance |-> 0;
 {
-    struct account myLocalAccount;
-    struct account *myAccount = &myLocalAccount;
-    if (myAccount == 0) { 
+    struct account *result = (struct account *)malloc(sizeof(struct account));
+    if (result == 0) { 
         abort();
     }
-    myAccount->balance = 5;
-    free(myAccount);
+    result->balance = 0;
+
+    return result;
+}
+
+void account_dispose(struct account *account)
+    //@ requires malloc_block_account(account) &*& account->balance |-> _;
+    //@ ensures true;
+{
+    free(account);
+}
+
+void account_set_balance(struct account *account, int newBalance)
+    //@ requires account->balance |-> _;
+    //@ ensures account->balance |-> newBalance;
+{
+    account->balance = newBalance;
+}
+
+int main()
+    //@ requires true;
+    //@ ensures true;
+{
+    struct account *myAccount = account_create();
+    account_set_balance(myAccount, 5);
+    account_dispose(myAccount);
     return 0;
 }
